389a4159870f9018ef2368a7ea2382601e800d97
tobecreative <smiledog@hotmail.com>
2020-01-16 14:18:07 -0500

    changes

--- app.js
+++ app.js
@@ -11,15 +11,14 @@ var mongoose = require("mongoose");
 var app = express();
 
 mongoose.connect(
-  "mongodb+srv://shadia:divinchi@cluster0-agoes.mongodb.net/sample_mflix?retryWrites=true&w=majority",
-  { useNewUrlParser: true },
-  function(err) {
-    if (err) {
-      console.log("error connecting", err);
-    } else {
-      console.log("connected");
+    "mongodb+srv://shadia:divinchi@cluster0-agoes.mongodb.net/sample_mflix?retryWrites=true&w=majority", { useNewUrlParser: true },
+    function(err) {
+        if (err) {
+            console.log("error connecting", err);
+        } else {
+            console.log("connected");
+        }
     }
-  }
 );
 
 // view engine setup
@@ -37,18 +36,18 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
